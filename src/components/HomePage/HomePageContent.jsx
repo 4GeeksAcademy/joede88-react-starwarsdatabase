07068cf31f5c3db7f6d5b4aa22cfb26ca74f9d24
Joe <joediaz88@hotmail.com>
2024-11-23 18:26:12 +0000

    creado mi primer boilerplate

---
+++ src/components/HomePage/HomePageContent.jsx
@@ -0,0 +1,14 @@
+import Alert from 'react-bootstrap/Alert';
+
+const HomePageContent = () => {
+    return (
+        <Alert variant="secondary">
+        <Alert.Heading>Hey, nice to see you</Alert.Heading>
+        <p>
+          Bienvenid@s a mi primer boilerplate. Aprovecha este proyecto para crear todo lo que se te ocurra.
+        </p>
+      </Alert>
+    )
+}
+
+export default HomePageContent
